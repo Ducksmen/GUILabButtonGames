cb4d39d8bf6ceb0e3373fd399142b72fd84520f0
BT_1N3_14 <jhoang6606@bths.edu>
2019-01-04 09:28:17 -0500

    a

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 
 
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,16 +17,43 @@ public class Controller
 {
     public Label Score;
     public Label txt;
+    public Label time;
     private int score = 0;
-    Timer timer = new Timer();
+    private boolean counting = false;
+    private boolean done = false;
 
-    TimerTask task = new TimerTask()
-    {
-
-    }
     public void pressButton(ActionEvent event)
     {
-        score++;
-        Score.setText("Score: " + String.valueOf(score));
+        if(!counting && !done)
+        {
+            long step = System.nanoTime() + 10000000000L;
+            viewTime();
+            new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    if(now > step)
+                    {
+                        Score.setText("You ended with " + score);
+                        counting = false;
+                        done = true;
+                        time.setText("Time Left: 0");
+                    }
+                    if (now < step) {
+                        long time2 = now - step;
+                        time.setText("Time Left: " + -time2);
+                    }
+                }
+            }.start();
+            counting = true;
+        }
+        if(counting) {
+            score++;
+            Score.setText("Score: " + String.valueOf(score));
+        }
+    }
+
+    public void viewTime()
+    {
+
     }
 }
